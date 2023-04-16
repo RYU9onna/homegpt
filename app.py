e3bd3f362fcc30339dc153e34faa587916d80df5
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-16 09:00:08 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json(force=True)
+    user_input = req["queryResult"]["queryText"]
+    response = generate_response(user_input)
+    return jsonify(response)
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    message = response.choices[0].text.strip()
+    return {"fulfillmentText": message}
+
+if __name__ == "__main__":
+    app.run(debug=True)
