57624b5ceaea161118fa4c85e920113092ff207e
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-17 08:23:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-import os
 from flask import Flask, request, jsonify
-import openai
+import json
 
 app = Flask(__name__)
-openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     req = request.get_json(force=True)
-    user_input = req["inputs"][0]["intent"]["query"]
-    response = generate_response(req, user_input)
-    return jsonify(response)
 
-def generate_response(req, prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"{prompt}\n\nChatbot:",
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    message = response.choices[0].text.strip()
+    # リクエストデータをログに出力
+    print("Request data:")
+    print(json.dumps(req, indent=2))
 
-    return {
-        "session": {
-            "id": req["session"]["id"],
-            "params": req["session"]["params"]
-        },
-        "prompt": {
-            "override": False,
-            "firstSimple": {
-                "speech": message,
-                "text": message
-            }
-        }
+    # 'inputs'キーが存在しない場合のデフォルト値を設定
+    user_input = "default_value"
+
+    # 'inputs'キーが存在する場合にのみ、値を取得
+    if "inputs" in req:
+        user_input = req["inputs"][0]["intent"]["query"]
+
+    # 以降の処理（例：GPT-4 による応答の生成）
+
+    response = {
+        "result": "success",
+        "message": f"User input: {user_input}"
     }
+
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
