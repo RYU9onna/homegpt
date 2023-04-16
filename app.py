09e2fdb851d4b8630d72b430e789e1f66e15efdc
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-17 05:58:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,25 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 def webhook():
     req = request.get_json(force=True)
     user_input = req.get("handler", {}).get("intent", {}).get("query", "")
-    intent_name = req.get("handler", {}).get("intent", {}).get("displayName", "")
-    
-    response = generate_response(intent_name, user_input)
+    response = generate_response(req, user_input)
     return jsonify(response)
 
-def generate_response(intent_name, prompt):
-    # ここでインテントを処理して、適切な応答を生成します。
-    if intent_name == "your_intent_name":
-        response_text = "your_response"
-    elif intent_name == "another_intent_name":
-        response_text = "another_response"
+def generate_response(req, prompt):
+    if prompt == "your_intent_name":
+        message = "your_response"
+    elif prompt == "another_intent_name":
+        message = "another_response"
     else:
-        response_text = "I'm not sure how to help with that."
-        
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=150,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+        message = response.choices[0].text.strip()
+
     return {
         "session": {
             "id": req["session"]["id"],
@@ -31,8 +36,8 @@ def generate_response(intent_name, prompt):
         "prompt": {
             "override": False,
             "firstSimple": {
-                "speech": response_text,
-                "text": response_text
+                "speech": message,
+                "text": message
             }
         }
     }
